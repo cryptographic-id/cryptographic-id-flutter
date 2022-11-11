c228919296e102619a508649a48bf42206cd7473
Simon Brand <simon.brand@postadigitale.de>
2022-11-11 23:20:32 +0000

    Move main back from ui/overview.dart

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import './ui/overview.dart';
+
+
+void main() {
+  runApp(const CryptographicID());
+}
+
+class CryptographicID extends StatelessWidget {
+  const CryptographicID({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cryptograhpic ID',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: AppLocalizations.supportedLocales,
+      home: const ContactOverview(title: 'Cryptograhpic ID'),
+    );
+  }
+}
--- lib/ui/overview.dart
+++ lib/ui/overview.dart
@@ -1,39 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import './qr_scan.dart';
-import './storage.dart';
-import './ui/loading_screen.dart';
-import './ui/scan_result.dart';
-import './ui/error_screen.dart';
+import '../qr_scan.dart';
+import '../storage.dart';
+import './loading_screen.dart';
+import './scan_result.dart';
+import './error_screen.dart';
 
-
-void main() {
-  runApp(const CryptographicID());
-}
-
-class CryptographicID extends StatelessWidget {
-  const CryptographicID({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Cryptograhpic ID',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-      ),
-      localizationsDelegates: [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: AppLocalizations.supportedLocales,
-      home: const ContactOverview(title: 'Cryptograhpic ID'),
-    );
-  }
-}
 
 class ContactOverview extends StatefulWidget {
   const ContactOverview({Key? key, required this.title}) : super(key: key);
