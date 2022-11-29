7d19c20484eeaae799ef519b16b9ff860f1462d3
Simon Brand <simon.brand@postadigitale.de>
2022-11-29 16:10:35 +0000

    qr_scan_free: add workaround to avoid black screen
    See: https://github.com/juliuscanute/qr_code_scanner/issues/415

--- lib/qr_scan_free.dart
+++ lib/qr_scan_free.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
@@ -23,29 +24,59 @@ Uint8List fixBrokenRawBytes(List<int> code) {
   return Uint8List.fromList(res.sublist(1, len1 + 1));
 }
 
-class ScanQR extends StatelessWidget {
+class ScanQR extends StatefulWidget {
   const ScanQR({Key? key, required this.title, required this.onScanned}) : super(key: key);
   final String title;
   final Function(BuildContext, Uint8List) onScanned;
 
+  @override
+  State<StatefulWidget> createState() => _ScanQRState();
+}
+
+
+class _ScanQRState extends State<ScanQR> {
+  QRViewController? controller;
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+
   void _onQRViewCreated(QRViewController controller, BuildContext context) {
+    setState(() {
+      this.controller = controller;
+    });
     controller.scannedDataStream.listen((scanData) {
       if (scanData.rawBytes == null) {
         debugPrint('Failed to scan Barcode');
       } else {
         final Uint8List code = fixBrokenRawBytes(scanData.rawBytes!);
         debugPrint('Barcode found! $code');
-        onScanned(context, code);
+        widget.onScanned(context, code);
       }
     });
   }
 
   @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller!.pauseCamera();
+    }
+    controller!.resumeCamera();
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final qrKey = GlobalKey();
+    if(controller != null && mounted) {
+      controller!.pauseCamera();
+      controller!.resumeCamera();
+    }
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
