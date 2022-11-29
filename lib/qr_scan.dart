72b7efa0a610c12a3aa4bf50e76ed499ec6f39b0
Simon Brand <simon.brand@postadigitale.de>
2022-11-29 01:34:40 +0000

    Split qr_scan into qr_scan_mlkit to add a free scanner for fdroid

--- lib/qr_scan.dart
+++ lib/qr_scan.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
-import 'package:mobile_scanner/mobile_scanner.dart';
+import './qr_scan_mlkit.dart';
 
 void scanQRCode(String purpose, BuildContext context,
                 Function(BuildContext, Uint8List) onScanned) async {
@@ -29,42 +29,4 @@ Future<Uint8List> scanQRCodeAsync(String purpose, BuildContext context) {
     }
   });
   return completer.future;
-}
-
-class ScanQR extends StatelessWidget {
-  const ScanQR({Key? key, required this.title, required this.onScanned}) : super(key: key);
-  final String title;
-  final Function(BuildContext, Uint8List) onScanned;
-
-  @override
-  Widget build(BuildContext context) {
-    final controller = MobileScannerController(
-      facing: CameraFacing.back,
-      torchEnabled: false);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            MobileScanner(
-              allowDuplicates: true, // otherwise binary qr code did not scan
-              controller: controller,
-              onDetect: (barcode, args) {
-                if (barcode.rawBytes == null) {
-                  debugPrint('Failed to scan Barcode');
-                } else {
-                  final Uint8List code = barcode.rawBytes!;
-                  debugPrint('Barcode found! $code');
-                  onScanned(context, code);
-                }
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
 }
